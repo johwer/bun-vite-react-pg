3a2d10aacb7b5fdb168145ae04cce4aadbec7050
Johan Wergelius <johanwergelius@gmail.com>
2024-05-16 12:48:14 +0200

    deploy to fly.io

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,32 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-const app = new Hono();
 import expensesRoute from "./routes/expenses";
+import { serveStatic } from "hono/bun";
 
+const app = new Hono();
 app.use("*", logger());
 
-app.get("/hello", (c) => c.text("Hello, Hono! 🚀"));
-app.get("/hello/:name", (c) => {
-  const name = c.req.param(`name`);
-  return c.text(`Hello, ${name}! 🚀`);
-});
-app.get("/test", (c) => c.json({ message: "Test" }));
+// app.get("/hello", (c) => c.text("Hello, Hono! 🚀"));
+// app.get("/hello/:name", (c) => {
+//   const name = c.req.param(`name`);
+//   return c.text(`Hello, ${name}! 🚀`);
+// });
+// app.get("/test", (c) => c.json({ message: "Test" }));
 
 app.route("/api/expenses", expensesRoute);
+
+//app.use("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
+
+app.get(
+  "*",
+  serveStatic({
+    onNotFound: (path, c) => {
+      console.log(`${path} is not found, you access ${c.req.path}`);
+    },
+  })
+);
+//app.use("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
 
 export default app;
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,7 @@
 import app from "./app";
 const server = Bun.serve({
+  hostname: "0.0.0.0",
+  port: process.env.PORT || 3000,
   fetch: app.fetch,
 });
 
